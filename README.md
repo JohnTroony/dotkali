6639f106ecb194411a0e678296a019d1bcaa67f9
root <root@localhost.localdomain>
2018-07-24 11:56:48 +0300

    mass mods

---
+++ README.md
@@ -0,0 +1,16 @@
+My i3wm config on Kali Linux
+-----
+
+### Installs:
+
+- Awesome and Yosemite San-Francisco Fonts
+- Moka Icons
+- IDA (free)
+- Sublime
+- Skype
+- Libreoffice
+- Hexchat
+	
+### Preview :
+
+![](./screenshots/3.png)
--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,6 @@ apt-get install dkms virtualbox-dkms
 apt-get install virtualbox
 
 
-
 # Install i3wm and related files
 apt-get install i3 -y 
 apt-get install i3lock i3status i3blocks -y
@@ -32,7 +31,6 @@ apt-get install devscripts arc-theme faba-icon-theme -y
 apt-get install arandr playerctl  quilt -y
 apt-get install freetype2-demos vflib3 libfreetype6-dev -y 
 
- 
 
 # Install Awesome and Yosemite San-Francisco Fonts
 mkdir ~/.fonts
@@ -45,34 +43,9 @@ dpkg -i src/debs/moka-icons.deb
 mkdir $baseDir
 cp -rf resourcefiles/* $baseDir
 cp -rf i3conf/*     $baseDir
+cp resourcefiles/scripts/lock /usr/bin/lock
 
 
-# Install Font enhancement
-cd $maindir && cd src/fontinstall
-
-git clone git://anongit.freedesktop.org/fontconfig
-cd fontconfig
-./autogen.sh
-make
-
-## Install any missing deps
-#cd freetype-infinality/
-#dpkg-checkbuilddeps
-
-cd ../fontconfig-infinality/
-dpkg-checkbuilddeps
-
-#cd ../freetype-infinality/
-#./build.sh
-
-#cd ../fontconfig-infinality/
-./build.sh
-
-# Install all the .deb files
-cd ../
-sudo dpkg -i \
-    #freetype-infinality/*.deb \
-    fontconfig-infinality/*.deb
 
 # Install IDA
 mkdir temp && cd temp
@@ -93,6 +66,10 @@ apt-get install libreoffice -y
 apt-get install hexchat terminator firefox-esr nautilus -y
 
 
-echo -e "Run the following after reboot : bash /etc/fonts/infinality/infctl.sh setstyle\n"
-echo -e "Edit /etc/profile.d/infinality-settings.sh \n"
+# GTK Config
+cp -r GTKconfs/gtk-2.0/ $baseDir
+cp -r GTKconfs/gtk-3.0/ $baseDir
+cp GTKconfs/gtk-2.0 /root/.gtk-2.0
+
+
 reboot
